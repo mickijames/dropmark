d9ae1f4f02c897d10055a9a053b30c4beab70974
mickijames <mwilliams27@vcd-ceb131-04.eastern.ewu.edu>
2017-05-09 16:42:17 -0700

    Version 1
    * [ ] need to do better version of showStuff for looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,47 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-console.log('Hello, World!');
+//********* Configure JSON Call ************//
+// The API Feed.
+var getStuff = 'http://mickijames.dropmark.com/420949.json';
+
+// Send back stuff. Private Collections.
+var setStuff = {};
+
+// Do stuff with returned data
+var doStuff = function(data) {
+
+  console.log(data.items);
+//  console.log(data.items[1].link);
+//  console.log(data.items[1].name);
+//  console.log(data.items[1].thumbnails.cropped);
+  
+  // What do you want to do? Show (display on page) data? 
+  showStuff(data);
+  
+};
+
+//********* Get & Set ************//
+$.getJSON(getStuff, setStuff, doStuff);
+
+//********* Display It ************//
+var showStuff = function(data) {
+    
+    // Put on Page
+    $('h1').text(data.name);
+    $('p a').attr('href', data.url);
+    
+    // Child Objects. May have many children.
+    // Loop or cycle through all the data
+    // Display data at each loop
+    $.each(data.items, function (i) {
+        
+        // loop
+        var stuff = '<p>' + data.items[i].name + '</p>';
+        
+        // put onto page
+        $('#items').append(stuff);
+        
+    });
+    
+};
